90717f53a357fba96a272a27ebb8540500d41192
selistym <selistym@protonmail.com>
2019-01-02 22:58:30 -0800

    component splitting

---
+++ src/SplitMe.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SplitMe = () => {
+  return <div>Call me, when needs</div>;
+};
+
+export default SplitMe;
--- src/notify.js
+++
@@ -1,4 +0,0 @@
-function notify(){
-    alert("happy new year!");    
-}
-export default notify;
